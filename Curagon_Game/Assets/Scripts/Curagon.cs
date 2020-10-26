88855ff9eab84d66685ad80a6a775cf71003e4fb
Callep3 <42645914+Callep3@users.noreply.github.com>
2020-10-26 13:44:09 +0100

    Added a Curagon
    A cube called Curagon has been added along with a almost blank script for curagon

---
+++ Curagon_Game/Assets/Scripts/Curagon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Curagon : MonoBehaviour
+{
+    public float maxHappiness = 100;
+    public float happiness;
+    public float maxHunger = 100;
+    public float hunger;
+    public float maxStamina = 100;
+    public float stamina;
+    private bool feed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (feed)
+        {
+            hunger += 10;
+            if (hunger >= maxHunger)
+            {
+                hunger = maxHunger;
+            }
+        }
+    }
+}
