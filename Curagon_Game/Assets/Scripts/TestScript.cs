9b5847c5a7b14de2b7d5e56a2fd1dd26dfbd330b
BakaRecca <eddie.norberg@gmail.com>
2020-10-26 13:40:58 +0100

    test

---
+++ Curagon_Game/Assets/Scripts/TestScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // This script is for testing gitHub.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
